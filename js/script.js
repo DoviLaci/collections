55a721cbb079485dc361fe3d68ae37340dec66de
DoviLaci <104922088+DoviLaci@users.noreply.github.com>
2023-08-30 21:26:36 +0200

    header finished-menu finished-searchbar finished

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log("Collections.js run");
+const navbarToggler = document.querySelector('.navbar__top--toggler');
+const navbarCollapse = document.querySelector('.navbar__collapse');
+
+navbarToggler.addEventListener('click', () => {
+    navbarToggler.classList.toggle('change');
+    navbarCollapse.classList.toggle('show__nav');
+});
